ce74659329a483715d75bbb88a13df392aaa849a
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-03 23:27:14 +0530

    update conditinals

---
+++ 1Students/student.c
@@ -0,0 +1,17 @@
+
+
+/*
+
+
+   1)  ruturaj 
+   2)  aditya 
+
+   3) rushikesh   one device
+   4) rupesh      one device
+   5) tushar 
+   6) abhisheikh
+
+   
+   
+
+*/
---
+++ day5_if_else/basic_conditions.c
@@ -0,0 +1,72 @@
+
+
+#include<stdio.h>
+
+
+void main() {
+
+   
+
+
+   /* 
+   
+      if() {}
+      
+      if() {}
+   
+
+      if() {}
+
+      else if() {}
+
+
+   
+   
+   */
+
+
+  /** 
+   * 
+   *    else if() {
+   *   }
+   * 
+   * 
+  */
+
+   
+
+ //  accept course price and check discount on it 
+
+   int price;   // variable declaration
+    
+   printf(" Enter course price");
+   
+   scanf("%d", &price);   /// initialization
+    
+    
+   if(5000 <= price) {
+      
+      // int discount =  price * (10 / 100);
+      
+      // int discountprice = price - discount;
+      
+      int discountPrice = price - 500;
+      
+      printf("after discounted 10 price is %d ", discountPrice);
+      
+   }
+   
+   else if(3000 <  price ) {
+      
+      printf(" after 5 percent discount %d", price - 200);
+      
+   }   
+   else {
+      printf(" price is very small ");
+   }
+    
+    
+
+
+}
+
