8811aa8f4c4451d8ed1704e09f1d1ac698b76dc7
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-03 22:09:09 +0530

    update the conditionals

---
+++ Operators/demo.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+void main() {
+
+
+    int a = 20;
+
+
+    printf(" %d ", a);
+
+    
+}
+
+
+
+
+
---
+++ conditionals/basic.c
@@ -0,0 +1,37 @@
+
+#include<stdio.h>
+
+
+void main() {
+
+int a;   /// variable declaration 
+
+
+printf("%d", a);
+
+
+a = 17;  /// variable initialization 
+
+
+printf("%d", a);
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+   return; // keyword 
+}
+
+
