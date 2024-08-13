b72a0e287e5172091a5eac4c037f12f2315d4cb4
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-13 14:30:01 +0530

    create the program for the loops

---
+++ day_8-loops/inputNumber.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+      int i=0,n;
+      printf("enter limit of loop");
+      scanf("%d",&n);
+      while (i<=n)
+      {
+            printf("%d\n",i);
+            i++;
+
+       }
+      
+
+
+
+
+
+
+}
---
+++ day_8-loops/while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+      int i=2;
+      do{
+      printf("%d\n",i);
+      i+=2;
+
+
+
+
+      }while(i<20);
+
+
+
+
+
+}
+
