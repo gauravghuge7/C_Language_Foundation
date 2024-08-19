8acdda179f9dca440e1157a42bed532b890279f7
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-19 12:33:27 +0530

    solve the code

---
+++ day_12_practice problems/basicfunction.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int addition(int a,int b){
+   printf("%d",a+b);
+
+   return 0;
+}
+
+
+int main (){
+   addition(5,10);
+   int c = 15;
+ return 0;
+}
---
+++ day_12_practice problems/substraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int substraction(int a,int b){
+
+   printf("%d",a-b);
+
+   return a-b;
+}
+
+
+ int main(){
+   substraction(5,3);
+
+   return 0;
+ }
