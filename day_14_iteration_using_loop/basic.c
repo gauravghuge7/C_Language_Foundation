09b1ce03a7a83722cf4e2e170177908487bc93c0
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-22 21:56:36 +0530

    solve the code

---
+++ day_14_iteration_using_loop/basic.c
@@ -0,0 +1,60 @@
+
+#include<stdio.h>
+
+void main() {
+
+
+   for(int i=18; i>=0; i--) {  // =    == 
+
+      printf("%d\n",i);
+
+   }
+
+
+
+
+   /* 
+   
+      int i=1;
+
+
+      for(;i<=10;) {
+      
+      
+         i++;
+
+      }
+
+
+      int i;
+
+      for(i=1; i<5;){
+      
+         i++;
+      }
+
+
+
+
+
+      for(int i=0; i<n; i++)  {
+      
+
+      }
+
+
+
+
+
+
+   
+   
+   
+   
+   */    
+
+
+
+
+
+}
