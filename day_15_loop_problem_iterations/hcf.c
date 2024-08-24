f8e9934274665588c75c72d92bb5a91c70cc0a59
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-24 22:02:42 +0530

    update the function code

---
+++ day_15_loop_problem_iterations/hcf.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+
+void main() {
+
+
+   
+   for(int i=1; i<=100; i++) {
+
+
+      if(i%5 == 0) {
+         printf("%d\n", i);
+      }
+
+
+   }
+
+}
+
+
+/*
+
+
+   step for creating a function 
+
+   
+   what is the type of function 
+
+   check if any parameters in function 
+
+
+
+   what should you return from the function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ day_16_function_declaration/demFunction.c
@@ -0,0 +1,60 @@
+
+#include<stdio.h>
+
+
+
+
+void addition() {
+
+
+}
+
+void ruturaj(int a, int b) {
+
+}
+
+
+void aditya(int a) {
+
+}
+
+
+
+void main() {
+
+   printf("%d", 17);
+
+   aditya(15);   // function calling
+
+   ruturaj(5,9);
+
+   
+}
+
+
+
+/** 
+ *    
+ * 
+ *    create a function that print your name 
+ *    
+ *    
+ *    create a function that give a number 
+ * 
+ *    create a function that accept two numbers and print the addition 
+ * 
+ *    create a function that accept two numbers and print the multiplication 
+ * 
+ *    create a function that accept two numbers and print the substraction 
+ * 
+ *    create a function that accept two numbers and print the division  
+ * 
+ *    create a function that accept two numbers and print the reminder  
+ *   
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
