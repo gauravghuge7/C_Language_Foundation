0fc552fef671b4412f92b3193ab6430c2b6f1a48
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-13 23:33:07 +0530

    add decimal to binary

---
+++ day_23_NumbserSystem/demo.c
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+
+void main() {
+
+   int a = 25;
+
+   int binary = 0;
+
+   while(a > 0) {
+
+      int remind = a % 2;
+
+      a = a / 2;
+
+      binary = binary * 10 + remind;
+
+   }
+
+
+   int inverse = 0;
+
+   while(binary > 0) {
+      int unit = binary % 10;
+
+      binary = binary / 10;
+
+      inverse = inverse * 10;
+
+      inverse = inverse + unit;
+   }
+
+
+   printf("binary = %d\n", inverse);
+
+}
