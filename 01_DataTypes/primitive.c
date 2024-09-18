52854c6b76fb47517980581e94d00ef3aad5f370
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-18 22:53:54 +0530

    start from data types

---
+++ 01_DataTypes/primitive.c
@@ -0,0 +1,62 @@
+
+#include <stdio.h>
+
+
+
+void main() {
+
+
+   /*   integer data type in c */
+   //    %d %i is the modifiers 
+
+   int coursePrice = 1500;
+//  |     |             |
+//  data  variable     value
+
+   printf("%d", coursePrice);
+
+
+   /*   float data type in c */
+
+   float floatPrice = 15.5;
+
+   printf("%f", floatPrice);
+
+
+   /*   char data type in c */
+
+   char charPrice = 'A';
+
+   printf("%c", charPrice);
+
+   /*   string data type in c */
+
+   char stringPrice[] = "A";
+
+   printf("%s", stringPrice);
+
+   /*   boolean data type in c */
+
+   int booleanPrice = 1;
+
+   printf("%d", booleanPrice);
+
+   
+   /** long data type in c */
+
+   long longPrice = 1000;
+
+   printf("%ld", longPrice);
+
+
+   /**  double data type in c */
+
+   double doublePrice = 10.5;
+
+   printf("%f", doublePrice);   
+
+
+
+
+
+}
