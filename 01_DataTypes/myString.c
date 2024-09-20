529790eb77da033c75b3dc3adcbbb12eff95214a
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-20 23:11:56 +0530

    create the coditionals

---
+++ 01_DataTypes/myString.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+   char *str = "Hello World";
+
+
+   printf("%c", str[0]);
+
+   return 0;
+   
+}
---
+++ 03_Conditionals/condition.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+
+int main() {
+
+   int greenTea = 25;
+
+   int tose = 10;
+
+   int bill = greenTea + tose;  // 25 + 10 = 35
+
+   if (bill < 50) {  // 25 <= 20
+      printf(" chai is cheap \n");
+   }
+   
+   else {
+      printf(" chai is very Expensive ");
+   }
+
+
+}
