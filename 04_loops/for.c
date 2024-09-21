c0f032b7defc57512abf7ee5cb4868fa293045a0
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-21 23:14:31 +0530

    push the loop code

---
+++ 04_loops/for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+
+
+   // i = i + 1    OR   i++   increment operator is used 
+   //                   ++i
+
+   // i = i + 1    OR   i--   decrement operator is used 
+   //                   --i
+
+   for (int i = 1; i <= 5; i = i + 1) {
+
+   }
+}
---
+++ 04_loops/while.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+
+int main() {
+
+   int i = 1;
+
+   while (i <= 5) {
+
+      printf("i = %d\n", i);
+
+      i = i * 2;
+   }
+
+}
