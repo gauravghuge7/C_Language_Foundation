9ca4ec567893208435d5f6cbdf2a0a12c97e4793
Gaurav Ghuge <guduughuge7@gmail.com>
2024-10-09 22:00:07 +0530

    update max number

--- 05_functions/diffrence.c
+++ 05_functions/diffrence.c
@@ -2,11 +2,19 @@
 
 #include <stdio.h>
 
+int square(int a) {
+ 	
+	int sq = a * a;
+
+	return sq;
+}
 
 
 
 int main() {
-   
-
-   
+	
+	int n = 15;
+	int sq = square(n);
+	
+	printf("square = %d", sq);    // output
 }
